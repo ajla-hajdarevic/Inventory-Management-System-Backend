10518a3672e6eb847faeb8e316d892c60216fa20
Zemka <azemina.celebic@stu.ssst.edu.ba>
2019-01-15 00:22:56 +0100

    Updated: prod controller and repository

--- src/main/java/com/example/pokusaj100/repository/ProductRepository.java
+++ src/main/java/com/example/pokusaj100/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.pokusaj100.repository;
 
 import com.example.pokusaj100.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {}
+public interface ProductRepository extends JpaRepository<Product, Integer> {}
