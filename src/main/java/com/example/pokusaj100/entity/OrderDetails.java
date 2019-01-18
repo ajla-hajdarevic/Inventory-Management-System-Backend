ffe1a40e86b97eed0c1e3b98da2792904e3fd187
Zemka <azemina.celebic@stu.ssst.edu.ba>
2019-01-18 01:53:26 +0100

    Added: CrossOrigin

--- src/main/java/com/example/pokusaj100/entity/OrderDetails.java
+++ src/main/java/com/example/pokusaj100/entity/OrderDetails.java
@@ -10,13 +10,10 @@ public class OrderDetails {
     @GeneratedValue
     private int id;
 
-    @ManyToOne
-    @JoinColumn(name = "supplier_id")
-    private Supplier supplier;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Order_ order_;
+    private int productId;
+    private int orderId;
+    private int orderedQuantity;
 
     public int getId() {
         return id;
@@ -26,19 +23,28 @@ public class OrderDetails {
         this.id = id;
     }
 
-    public Supplier getSupplier() {
-        return supplier;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setSupplier(Supplier supplier) {
-        this.supplier = supplier;
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
-    public Order_ getOrder() {
-        return order_;
+    public int getOrderId() {
+        return orderId;
     }
 
-    public void setOrder(Order_ order) {
-        this.order_ = order;
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
     }
+
+    public int getOrderedQuantity() {
+        return orderedQuantity;
+    }
+
+    public void setOrderedQuantity(int orderedQuantity) {
+        this.orderedQuantity = orderedQuantity;
+    }
+
 }
